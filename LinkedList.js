fbe85f278fb57cf6053bf4f6dfe7f1b001120b8f
Ray Huber <raymond@raymondhuber.com>
2016-08-08 19:54:00 -0400

    Finished LikedList

--- LinkedList.js
+++ LinkedList.js
@@ -66,29 +66,63 @@ LinkedList.prototype.contains = function(val) {
 };
 
 LinkedList.prototype.remove = function(val) {
+  //check if the value is even in the list
   if (!this.contains(val)){
     return;
   }
+  //if it's the first node, just delete and move head up
   if (this.head.data === val){
     this.head = this.head.next;
     return;
   }
+
+  //if it's in the middle, change the next of preious to current
+  var prev = null;
+  var current = this.head;
+
+  while (current.data !== val){
+    prev = current;
+    current = current.next;
+  }
+
+  //now close the gap
+  prev.next = current.next;
 };
 
 LinkedList.prototype.contains = function(val) {
   var current = this.head;
-  while(current.val !== null){
-    if (current.val === val){
+  while(current !== null){
+    if (current.data === val){
       return true;
     }
-    else{
-      return false;
-    }
+    current = current.next;
   }
+  return false;
 };
 
+LinkedList.prototype.print = function() {
+  output = "[";
+  current = this.head;
+  while (current !== null){
+    output += current.data;
+    if(current.next !== null){
+      output += ",";
+    }
+    current = current.next;
+  }
+  output += "]";
+  console.log(output);
+};
+
+
+//TESTS
 var list = new LinkedList();
-
-list.prepend(10);
-
-console.log(list);
+list.append(5);
+list.append(10);
+list.append(15);
+list.append(20);
+console.log(list.contains(10));
+console.log(list.contains(8));
+list.remove(10);
+list.prepend("blah");
+list.print();
